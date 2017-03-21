3a5df161ae760117b6d1e486e516f580db15c887
Liang Xingchen <liang@maichong.it>
2017-03-21 20:50:55 +0800

    0.3.0

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 var akita = require('akita');
 var fetch = require('node-fetch');
+var FormData = require('form-data');
 
-akita.setOptions({ fetch: fetch });
+akita.setOptions({ fetch: fetch, FormData: FormData });
 
 var create = akita.create;
 var resolve = akita.resolve;
@@ -13,6 +14,9 @@ function newCreate(options) {
   if (!options.fetch) {
     options.fetch = fetch;
   }
+  if (!options.FormData) {
+    options.FormData = FormData;
+  }
   let client = create(options);
   client.create = newCreate;
   client.resolve = newResolve;
@@ -21,9 +25,15 @@ function newCreate(options) {
 
 function newResolve() {
   let client = resolve.apply(this, arguments);
-  if (!client._count && !client._options.fetch) {
+  if (!client._count) {
     // 还未发送请求，新实例
-    client.setOptions({ fetch: fetch });
+    if (!client._options.fetch) {
+      client.setOptions({ fetch: fetch });
+    }
+    if (!client._options.FormData) {
+      // 还未发送请求，新实例
+      client.setOptions({ FormData: FormData });
+    }
   }
   client.create = newCreate;
   client.resolve = newResolve;
